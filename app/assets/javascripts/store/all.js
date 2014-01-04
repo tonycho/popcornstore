8e21ee01974c2a7b77eae2d12b636b73aea7ecd5
Tony <tony.cho@gmail.com>
2014-01-04 00:14:13 -0800

    updated js

--- app/assets/javascripts/store/all.js
+++ app/assets/javascripts/store/all.js
@@ -3,6 +3,7 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
+// new updated
 //
 //= require jquery
 //= require jquery_ujs
@@ -10,3 +11,5 @@
 //= require store/spree_frontend
 
 //= require_tree .
+
+
