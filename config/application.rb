3139dad32a80f90798075a66cd1a4c45d6e53f5d
Tony <tony.cho@gmail.com>
2014-01-04 00:35:08 -0800

    added precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Store
   class Application < Rails::Application
     #config.assets.initialize_on_precompile = false
 
-    #config.assets.manifest = "/assets"
-    config.assets.enabled = false
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
