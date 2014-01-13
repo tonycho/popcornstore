94aca3503ccffecee9de7d221617d5ec8fc0b8e5
Tony <tony.cho@gmail.com>
2014-01-12 18:05:56 -0800

    updated css

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,6 +18,8 @@ end
 
 Spree.user_class = "Spree::User"
 
+Spree::Auth::Config[:registration_step] = false;
+
 Paperclip.interpolates(:s3_eu_url) do |attachment, style|
   "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
 end
