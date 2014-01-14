e76da38a568895f3467d8928adb0440dbecc96b1
Tony <tony.cho@gmail.com>
2014-01-14 00:50:48 -0800

    added breadcrumbs back

--- app/helpers/base_helper_decorator.rb
+++ app/helpers/base_helper_decorator.rb
@@ -16,5 +16,54 @@ module Spree
 
       link_to text, spree.cart_path, :class => "#{css_class}"
     end
+
+    def breadcrumbs(taxon = nil, product = nil, sep = "&nbsp;&raquo;&nbsp;")
+      if String === product
+   			sep = product
+   			product = nil
+   		end
+
+   		return "" unless taxon || product || current_page?(products_path)
+
+   		session['last_crumb'] = taxon ? taxon.permalink : nil
+   		sep = raw(sep)
+   		crumbs = [] #[content_tag(:li, link_to(t(:home) , root_path) + sep)]
+
+   		if taxon
+
+        unless taxon.ancestors.empty?
+          taxon.ancestors.each do |ancestor|
+            unless ancestor.name == "Brand"
+              crumbs << content_tag(:li, link_to(ancestor.name , seo_url(ancestor)) + sep)
+            end
+          end
+        end
+
+   			if product
+   				crumbs << content_tag(:li, link_to(taxon.name , seo_url(taxon)) + sep)
+   				crumbs << content_tag(:li, content_tag(:span, product.name))
+   			else
+   				crumbs << content_tag(:li, content_tag(:span, taxon.name))
+   			end
+   		elsif product
+   			crumbs << content_tag(:li, link_to(t('products') , products_path) + sep)
+   			crumbs << content_tag(:li, content_tag(:span, product.name))
+   		else
+   			crumbs << content_tag(:li, content_tag(:span, t('products')))
+   		end
+   		crumb_list = content_tag(:ul, raw(crumbs.flatten.map{|li| li.mb_chars}.join), :class => 'inline')
+   		content_tag(:div, crumb_list, :id => 'breadcrumbs')
+   	end
+
+   	def last_crumb_path
+   		plink = session['last_crumb']
+   		if plink && taxon = Spree::Taxon.find_by_permalink(plink)
+   			seo_url(taxon)
+   		else
+   			products_path
+   		end
+   	end
+
   end
 end
+
